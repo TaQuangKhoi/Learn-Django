53faebdf8cf34f7f1efaf449472b537534781af8
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-21 00:02:48 +0700

    install playground in storefront

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -34,9 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.sessions', # can be removed if not using sessions
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playground'
 ]
 
 MIDDLEWARE = [
