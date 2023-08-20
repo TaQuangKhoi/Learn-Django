5b13ce386a79d38f07457de548834f4c8a9481d2
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-21 00:16:11 +0700

    Chào Hảo

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def say_hello(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'hello.html', {'name': 'Hảo'})
