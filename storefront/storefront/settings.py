5211d3009338f43f40715833323ca63d6871462e
Tạ Quang Khôi <khoi0941@gmail.com>
2023-08-22 00:09:14 +0700

    install django debug toolbar in storefront

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions', # can be removed if not using sessions
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+
+    'playground',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'storefront.urls'
@@ -122,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
