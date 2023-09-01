9728f819db7292a6e23e4d821b400e3a1cd9d420
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-01 18:21:09 +0700

    add some function app

--- mailapp/app/models.py
+++ mailapp/app/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(null=True)
+    bio = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Messages(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    message = models.TextField(null=True)
 
     def __str__(self):
         return self.name
