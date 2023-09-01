5286be63438d8afc0552b9427c30b33c50d5c6b4
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-01 23:44:04 +0700

    write fucntion clickFolderItem

--- mailapp/mailapp/settings.py
+++ mailapp/mailapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*llm_f@bf8oqc9&ug&!mrztt_dnl925qdk%=6t+m@h@b#+d7qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
