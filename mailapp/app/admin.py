bfb19827cbe2b1d56b5ed1dd7eaf3e87f659e591
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-01 18:26:33 +0700

    format account-container

--- mailapp/app/admin.py
+++ mailapp/app/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Author, Messages
 
-admin.site.register(Author)
-admin.site.register(Messages)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "bio",)
+
+
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ("name", "author", "message",)
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Messages, MessagesAdmin)
