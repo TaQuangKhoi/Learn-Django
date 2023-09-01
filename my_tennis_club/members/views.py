0821caec854bf5495a95817b406388d65c37918f
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-01 15:26:31 +0700

    install 'members' for more complicated stuff

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 
 # Create your views here.
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
