f2a748ab0d5fdac8d3220ac65f2212f447679496
Tạ Quang Khôi <khoi0941@gmail.com>
2023-09-01 15:53:23 +0700

    Show Member list

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
+
+from members.models import Member
 
 
 # Create your views here.
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    my_members = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'my_members': my_members,
+    }
+    return HttpResponse(template.render(context, request))
