818c289e3141f46bec27f07873657158bc0dfbec
Tạ Quang Khôi <khoi0941@gmail.com>
2023-10-16 13:53:56 +0700

    Done crate project learn_advanced

--- learn-advanced/learn_advanced/learn_advanced/urls.py
+++ learn-advanced/learn_advanced/learn_advanced/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for test2 project.
+URL configuration for learn_advanced project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
