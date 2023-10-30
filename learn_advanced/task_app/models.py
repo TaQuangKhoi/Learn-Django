900b16f734cf79dbd5b1f29bbb4ae12dc71a45e5
Tạ Quang Khôi <khoi0941@gmail.com>
2023-10-30 13:21:19 +0700

    Switch to use task_app

--- learn_advanced/task_app/models.py
+++ learn_advanced/task_app/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
+
+
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='uploads/avatars/%Y/%m')
 
